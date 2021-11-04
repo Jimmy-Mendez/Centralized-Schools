a23d9997c95211fe8b934c5ac1dfbc6c8bb72002
Jimmy-Mendez <jmen34956@gmail.com>
2021-11-04 09:18:03 -0400

    first commit

---
+++ tests.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+  string data1, data2;
+  cout<<"Enter the data for the student preferences in the following format: \n student \t choice 1 \t choice 2 \t ... \t choice n \t "<<endl;
+  cin>>data1;
+  cout<<"Enter the data for the student preferences in the following format: \n student \t choice 1 \t choice 2 \t...\t choice n \t"<<endl;
+  cin>>data2;
+}
