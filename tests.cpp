f8f49d4afa21d9999e9493f09b783762a1c793e7
Jimmy Mendez <54858901+Jimmy-Mendez@users.noreply.github.com>
2021-11-11 18:09:52 -0500

    create match function

--- tests.cpp
+++ tests.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
+vector<string> match(vector<string> students, vector<string> student_pref, vector<string> schools, vector<string> school_pref);
+
 
 int main(){
   string data1, data2;
@@ -10,3 +13,10 @@ int main(){
   cout<<"Enter the data for the student preferences in the following format: \n student \t choice 1 \t choice 2 \t...\t choice n \t"<<endl;
   cin>>data2;
 }
+
+vector<string> match(vector<string> students, vector<string> student_pref, vector<string> schools, vector<string> school_pref)
+{
+	//use vectors to match using some mechanism
+
+}
+
