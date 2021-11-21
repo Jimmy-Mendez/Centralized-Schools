8551c9659747efb2b7b9d4275bf1e6ecdc564e96
Jimmy Mendez <54858901+Jimmy-Mendez@users.noreply.github.com>
2021-11-20 22:32:32 -0500

    input

--- tests.cpp
+++ tests.cpp
@@ -8,11 +8,13 @@ vector<string> match(vector<string> students, vector<string> student_pref, vecto
 
 int main(){
   int num1, num2;
-  vector<string> data1, data2, data3, data4;
+  vector<string> data1, data2;
+
+  //get student preferences
   cout<<"Enter number of students"<<endl;
   cin>>num1;
   cout<<"Enter students in order"<<endl;
-  
+
   for (int i = 0; i < num1; i++) 
   {
 	string input;
@@ -23,13 +25,22 @@ int main(){
   {
     cout<<data1[j]<<endl;
   }
-  // cout<<"Enter the data for the student preferences in the following format: \n student \t choice 1 \t choice 2 \t...\t choice n \t"<<endl;
-  // cin>>data2;
-  // cout<<"Enter schools in order"<<endl;
-  // cin>>data3;
-  // cout<<"Enter the data for the school preferences in the following format: \n student \t choice 1 \t choice 2 \t...\t choice n \t"<<endl;
-  // cin>>data4;
-  // cout<<"The outcome is: " << match(data1, data2,data3,data4) << endl;;
+
+  // Get school preferences
+  cout << "Enter number of schools" << endl;
+  cin >> num2;
+  cout << "Enter schools in order" << endl;
+
+  for (int i = 0; i < num2; i++)
+  {
+	  string input;
+	  data2.push_back(input);
+	  cin >> input;
+  }
+  for (int j = 0; j < num2; j++)
+  {
+	  cout << data2[j] << endl;
+  }
    return 0;
 }
 
